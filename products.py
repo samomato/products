8e06a5570c8aa0185d1a376e8488b58298ba27c1
Reeve Hou <sam2972570@gmail.com>
2023-08-25 02:38:44 +0800

    add type transform and header

--- products.py
+++ products.py
@@ -4,5 +4,12 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
+	price = int(price)
 	Pro.append([name, price])
 print(Pro)
+
+# 以下寫入檔案
+with open('products.csv', 'w', encoding='utf-8') as pf:
+	pf.write('商品,價格\n')
+	for p in Pro:
+		pf.write(p[0] + ',' + str(p[1]) + '\n')
